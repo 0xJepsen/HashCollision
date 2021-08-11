e102b6b19361839a050df365152f3139e60877d5
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2021-08-11 10:20:43 -0600

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,23 @@
+from eth_account import Account
+import secrets
+from etherscan import Etherscan
+eth = Etherscan("KKRW7D1SQKAG2KNEBZA4XNR68H9MWYNMIB")
+import json
+import pprint
+import time
+
+count = 0;
+while True:
+    priv = secrets.token_hex(32)
+    private_key = "0x" + priv
+    acct = Account.from_key(private_key)
+    if (count%5==0):
+        time.sleep(1)
+    balance = eth.get_eth_balance(acct.address)
+    print("balance of wallet {} is {}".format(acct.address, balance))
+    if (balance != '0'):
+        print("FOUND ETH IS WALLET with pub key: {}".format(acct.address))
+        break
+    count +=1 
+
+print("checked: {} random addresses".format(count))
